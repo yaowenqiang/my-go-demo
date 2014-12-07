81fec3421a3d27e35869d2621e9775c1fe995635
yaowenqiang <yaowenqiang111@163.com>
2014-12-07 14:42:27 +0000

    change the function paramter from array to slices

--- src/array/array.go
+++ src/array/array.go
@@ -2,12 +2,23 @@ package main
 import (
     "fmt"
 )
-func printArr(w [4]string) {
+
+/* func printArr(w [4]string) { */
+/*     for _,word := range w { */
+/*         fmt.Printf("%s ",word) */
+/*     } */
+/*     fmt.Printf("\n") */
+/*     w[3] = "blue" */
+/* } */
+
+/* slices */
+
+func printArr(w []string) {
     for _,word := range w {
         fmt.Printf("%s ",word)
     }
     fmt.Printf("\n")
-    w[3] = "blue"
+    w[2] = "blue"
 }
 func main (){
     /* words := [...]string{ "the","quick","fox" } */
@@ -17,7 +28,10 @@ func main (){
     /* printArr(words); */
     /* printArr(words); */
     /* fmt.Printf("%s\n",words[1]) */
-    fmt.Printf("%d \n",len(words))
+    /* fmt.Printf("%d \n",len(words)) */
+    /* fmt.Printf("%d \n",len(words)) */
+    printArr(words);
+    printArr(words);
 
 }
 
