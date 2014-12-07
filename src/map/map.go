1a8d3e1f7a04f25b06526521dfed09d9c282e2cb
yaowenqiang <yaowenqiang111@163.com>
2014-12-07 18:37:37 +0000

    add another map create method

--- src/map/map.go
+++ src/map/map.go
@@ -4,21 +4,36 @@ import(
 )
 
 func main () {
-    dayMounths := make(map[string]int)
-    dayMounths["Jan"] = 31
-    dayMounths["Feb"] = 28
-    dayMounths["Mar"] = 31
-    dayMounths["Apr"] = 30
+    /* dayMounths := make(map[string]int) */
+    /* dayMounths["Jan"] = 31 */
+    /* dayMounths["Feb"] = 28 */
+    /* dayMounths["Mar"] = 31 */
+    /* dayMounths["Apr"] = 30 */
 
-    dayMounths["May"] = 31
-    dayMounths["Jun"] = 30
-    dayMounths["Jul"] = 31
-    dayMounths["Agu"] = 31
+    /* dayMounths["May"] = 31 */
+    /* dayMounths["Jun"] = 30 */
+    /* dayMounths["Jul"] = 31 */
+    /* dayMounths["Agu"] = 31 */
 
-    dayMounths["Sep"] = 30
-    dayMounths["Oct"] = 31
-    dayMounths["Nov"] = 30
-    dayMounths["Dec"] = 31
+    /* dayMounths["Sep"] = 30 */
+    /* dayMounths["Oct"] = 31 */
+    /* dayMounths["Nov"] = 30 */
+    /* dayMounths["Dec"] = 31 */
+
+    dayMounths:= map[string]int{
+        "Jan":31,
+        "Feb":28,
+        "Mar":31,
+        "Apr":30,
+        "May":31,
+        "Jun":30,
+        "Jul":31,
+        "Agu":31,
+        "Sep":30,
+        "Oct":31,
+        "Nov":30,
+        "Dec":31,
+    }
 
     /* fmt.Printf("Days in February: %d \n",dayMounths["Feb"]) */
     /* days,OK := dayMounths["January"] */
@@ -33,8 +48,8 @@ func main () {
         fmt.Printf("%s has % days\n",month,days)
 
     }
-    delete(dayMounths,"Feb")
-    delete(dayMounths,"Mar")
+    /* delete(dayMounths,"Feb") */
+    /* delete(dayMounths,"Mar") */
     day31 := 0
     for _,days := range dayMounths {
         if days == 31 {
