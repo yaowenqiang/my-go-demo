9130c783b3e0101152f51ae5087cc1374eda0793
yaowenqiang <yaowenqiang111@163.com>
2014-12-07 14:54:09 +0000

    add array append example

--- src/array/array.go
+++ src/array/array.go
@@ -23,15 +23,30 @@ func printArr(w []string) {
 func main (){
     /* words := [...]string{ "the","quick","fox" } */
     /* words := [4]string{ "the","quick","fox" } */
-    words := []string{ "the","quick","brown","fox","jump","over","the","lazy","dog"}
+    /* words := []string{ "the","quick","brown","fox","jump","over","the","lazy","dog"} */
     /* printArr(words); */
     /* printArr(words); */
     /* printArr(words); */
     /* fmt.Printf("%s\n",words[1]) */
     /* fmt.Printf("%d \n",len(words)) */
     /* fmt.Printf("%d \n",len(words)) */
-    printArr(words);
-    printArr(words);
+    /* printArr(words); */
+    /* printArr(words); */
+    /* printArr(words[1:7]); */
+    /* printArr(words[5:len(words)]); */
+    /* printArr(words[5:]); */
+    /* printArr(words[:9]); */
+    /* words := make([]string,4) */
+    words := make([]string,0,4)
+    words = append(words,"The")
+    words = append(words,"Quick")
+    words = append(words,"Brown")
+    words = append(words,"Fox")
+    /* words[0] = "The" */
+    /* words[1] = "Quick" */
+    /* words[2] = "Brown" */
+    /* words[3] = "Fox" */
+    printArr(words)
 
 }
 
