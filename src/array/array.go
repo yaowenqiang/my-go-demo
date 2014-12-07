f94187ea2931c7ae3d87edaebeafbbadb616af9f
yaowenqiang <yaowenqiang111@163.com>
2014-12-07 08:56:16 +0000

    add the print array function

--- src/array/array.go
+++ src/array/array.go
@@ -2,9 +2,17 @@ package main
 import (
     "fmt"
 )
+func printArr(w [4]string) {
+    for _,word := range w {
+        fmt.Printf("%s ",word)
+    }
+    fmt.Printf("\n")
+}
 func main (){
-    words := [...]string{ "the","quick","fox" }
-    fmt.Printf("%s\n",words[1])
+    /* words := [...]string{ "the","quick","fox" } */
+    words := [4]string{ "the","quick","fox" }
+    printArr(words);
+    /* fmt.Printf("%s\n",words[1]) */
 
 }
 
