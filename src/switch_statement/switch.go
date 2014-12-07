f31e6b85425d20eccaaf812e7a4434989b41396f
yaowenqiang <yaowenqiang111@163.com>
2014-12-07 06:59:43 +0000

    add the fallthrough example

--- src/switch_statement/switch.go
+++ src/switch_statement/switch.go
@@ -7,13 +7,15 @@ import (
 
 func main (){
     n,err := fmt.Printf("hello world!\n");
+    n = 0
     switch{
         case err != nil:
             os.Exit(1)
         case n == 0:
-            fmt.Printf("No bytes output")
-        case n != 14:
-            fmt.Printf("Wrong number of characters!")
+            fmt.Printf("No bytes output\n")
+            fallthrough
+        case n != 13:
+            fmt.Printf("Wrong number of characters! %d",n)
         default:
             fmt.Printf("OK!")
     }
