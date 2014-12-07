1d57e3752e249760623b1a230db091c4b095f0b3
yaowenqiang <yaowenqiang111@163.com>
2014-12-07 07:07:05 +0000

    add the atoz example

--- src/switch_statement/switch.go
+++ src/switch_statement/switch.go
@@ -20,5 +20,17 @@ func main (){
             fmt.Printf("OK!")
     }
     fmt.Printf("\n")
+    atoz := "the quick brown fox jumps over the lazy dog"
+    vowels := 0
+    consonants := 0
+    for _,f := range atoz {
+        switch f {
+            case 'a','e','i','o','u':
+                vowels +=1
+            default:
+                consonants +=1
+        }
+    }
+    fmt.Printf("Vowels: %d,Consonants: %d\n",vowels,consonants)
 }
 
