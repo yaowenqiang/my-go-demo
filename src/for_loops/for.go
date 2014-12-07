4f54387b53dd83965c119c79b3bf323a4942e3ae
yaowenqiang <yaowenqiang111@163.com>
2014-12-07 07:22:25 +0000

    add the c for like style demo

--- src/for_loops/for.go
+++ src/for_loops/for.go
@@ -4,9 +4,14 @@ import (
     /* "os" */
 )
 func main (){
-    for {
+    /* var counter int */
+    /* counter = 0 */
+    /* for counter < 10 { */
+    /*     fmt.Printf("Hello world!\n") */
+    /*     counter ++ */
+    /* } */
+    for i :=0;i < 10 ;i++ {
         fmt.Printf("Hello world!\n")
-
     }
 
 }
