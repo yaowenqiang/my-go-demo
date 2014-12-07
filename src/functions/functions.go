701da3860e577326f3626ff854a3937dce38dd84
yaowenqiang <yaowenqiang111@163.com>
2014-12-07 07:57:23 +0000

    add the return value to the functions

--- src/functions/functions.go
+++ src/functions/functions.go
@@ -4,13 +4,19 @@ import (
 )
 
 /* func printer(message1,message2 string,string) { */
-func printer(message1,message2 string,repeat int) {
-    for repeat>0 {
-    fmt.Printf("%s %s\n",message1,message2)
-    repeat --
-    }
+/* func printer(message1,message2 string,repeat int) error { */
+/*     for repeat>0 { */
+/*     fmt.Printf("%s %s\n",message1,message2) */
+/*     repeat -- */
+/*     } */
+/* } */
+
+func printer(message string) error {
+    _,error := fmt.Printf("%s\n",message)
+    return error
 }
 func main (){
-    printer("Hello"," world",10)
+    /* printer("Hello") */
+    printer("Hello")
 
 }
