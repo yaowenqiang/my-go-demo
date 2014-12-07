651aa282e2f2e31ca637ecf2a4fef71ef28ec194
yaowenqiang <yaowenqiang111@163.com>
2014-12-07 07:53:02 +0000

    add the repeat parameter

--- src/functions/functions.go
+++ src/functions/functions.go
@@ -4,10 +4,13 @@ import (
 )
 
 /* func printer(message1,message2 string,string) { */
-func printer(message1,message2 string) {
+func printer(message1,message2 string,repeat int) {
+    for repeat>0 {
     fmt.Printf("%s %s\n",message1,message2)
+    repeat --
+    }
 }
 func main (){
-    printer("Hello"," world")
+    printer("Hello"," world",10)
 
 }
