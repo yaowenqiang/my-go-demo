457a0a3b38c89ab7ef423012ce706c5abc3c832d
yaowenqiang <yaowenqiang111@163.com>
2014-12-09 17:19:40 +0000

    add the write file block

--- src/byteslice/byteslice.go
+++ src/byteslice/byteslice.go
@@ -11,13 +11,17 @@ func main (){
         fmt.Printf("%s\n",err)
         os.Exit(1)
     }
-    defer f.Close()
     b := make([]byte,100)
 
     n,err := f.Read(b)
     /* fmt.Printf("%d: % x\n",n,b) */
     /* fmt.Printf("%d: % s\n",n,b) */
-    fmt.Printf("%d: % c\n",n,b)
-    /* stringVersion := string(b) */
+    /* fmt.Printf("%d: % c\n",n,b) */
+    stringVersion := string(b)
     fmt.Printf("%d: % s\n",n,stringVersion)
+    someString := "some string"
+    /* f.Write(someString) */
+    _,err = f.Write([]byte(someString))
+    fmt.Printf("%s\n",err)
+    defer f.Close()
 }
