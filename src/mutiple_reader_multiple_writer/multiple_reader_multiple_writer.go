0c057e0d282a4d20ad11b9b5c28233bc364c4fd2
yaowenqiang <yaowenqiang111@163.com>
2014-12-14 15:35:47 +0000

    add the getter function and use channel

--- src/mutiple_reader_multiple_writer/multiple_reader_multiple_writer.go
+++ src/mutiple_reader_multiple_writer/multiple_reader_multiple_writer.go
@@ -20,16 +20,33 @@ func getPage(url string) (int,error){
     return len(body),nil
 
 }
+func getter (url string,size chan int) {
+    length,err := getPage(url)
+    if err == nil {
+        size <- length
+    } else {
+        os.Exit(1)
+
+    }
+
+}
 func main () {
     urls := []string{"http://baidu.com/","http://www.sohu.com/","http://www.163.com/"}
-    for _,url := range urls {
-        pageLength,err := getPage(url)
-        if err != nil {
-            os.Exit(0);
+    /* for _,url := range urls { */
+    /*     pageLength,err := getPage(url) */
+    /*     if err != nil { */
+    /*         os.Exit(0); */
 
-        }
-        fmt.Printf("%s is length %d \n",url,pageLength)
+    /*     } */
+    /*     fmt.Printf("%s is length %d \n",url,pageLength) */
 
+    /* } */
+    size := make(chan int)
+    for _,url := range urls{
+        go getter(url,size)
+    }
+    for i := 0;i<3; i++ {
+        fmt.Printf("%d \n",<-size)
     }
 }
 
