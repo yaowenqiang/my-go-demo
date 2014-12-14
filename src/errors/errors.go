93c965c1850bcb580d9867622b8a966f7f2f56da
yaowenqiang <yaowenqiang111@163.com>
2014-12-14 07:28:10 +0000

    add the errors.new method

--- src/errors/errors.go
+++ src/errors/errors.go
@@ -2,11 +2,16 @@ package main
 import (
     "fmt"
     "os"
+    "errors"
+)
+
+var (
+    errorEmptyString = errors.New("Unwilling to print an epty string")
 )
 func printer(msg string) error {
     if msg == "" {
-        return fmt.Errorf("Unwilling to print an empty string");
-
+        /* return fmt.Errorf("Unwilling to print an empty string"); */
+        return errorEmptyString
     }
     _,err := fmt.Printf("%s\n",msg)
     return err
@@ -19,7 +24,11 @@ func main () {
     /* if  error := printer("Hello World!"); error != nil { */
     if  error := printer(""); error != nil {
         
-        fmt.Printf("Printer say :failed: %s\n",error);
+        if  error == errorEmptyString {
+            fmt.Printf("You tried to print an empty string!")
+        } else {
+            fmt.Printf("Printer say :failed: %s\n",error);
+        }
         os.Exit(1);
     }
     printer("");
