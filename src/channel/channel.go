8579c2c1a8e0c5066f54c5ef59496750f6eb7ccb
yaowenqiang <yaowenqiang111@163.com>
2014-12-14 10:32:21 +0000

    add the rand integer channel demo

--- src/channel/channel.go
+++ src/channel/channel.go
@@ -1,22 +1,44 @@
 package main
 import (
     "fmt"
+    "math/rand"
 )
-func emit(c chan string) {
-    words := []string{"The","Quick","Brown","Fox"}
-    for _,word := range words {
+/* func emit(c chan string) { */
+/*     words := []string{"The","Quick","Brown","Fox"} */
+/*     for _,word := range words { */
         
-        c <- word
-    }
-    close(c)
-}
-func main () {
-    wordChannel := make(chan string)
-    go emit(wordChannel)
-    for word := range wordChannel {
-        fmt.Printf("%s ",word)
+/*         c <- word */
+/*     } */
+/*     /1* close(c) *1/ */
+/* } */
+func makeRadoms(c chan int) {
+    for {
+        c <- rand.Intn(2000)
 
     }
-    fmt.Printf("\n")
+}
+func main () {
+    /* wordChannel := make(chan string) */
+    randoms := make(chan int)
+    go makeRadoms(randoms)
+    /* go emit(wordChannel) */
+    for n := range randoms {
+        fmt.Printf("%d ",n)
+
+    }
+
+    /* fmt.Printf("\n") */
+    /* word := <- wordChannel */
+    /* fmt.Printf("%s \n",word) */
+    /* word = <- wordChannel */
+    /* fmt.Printf("%s \n",word) */
+    /* word = <- wordChannel */
+    /* fmt.Printf("%s \n",word) */
+    /* word = <- wordChannel */
+    /* fmt.Printf("%s \n",word) */
+    /* /1* var ok bool *1/ */
+    /* /1* word,ok = <- wordChannel *1/ */
+    /* word,ok := <- wordChannel */
+    /* fmt.Printf("%s %t\n",word,ok) */
 }
 
